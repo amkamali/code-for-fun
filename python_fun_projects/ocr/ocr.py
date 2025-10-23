e4349e0543bfe542b200e3ca63c7d299dd36045d
amir-kamali <amir@plego.com>
2025-10-22 21:17:06 -0700

    Adding option of passing jpg file to ocr.py + defining the git ignore file types

--- python_fun_projects/ocr/ocr.py
+++ python_fun_projects/ocr/ocr.py
@@ -1,8 +1,16 @@
+import argparse
+from pathlib import Path
 from PIL import Image
 import pytesseract, re
+import sys
 
-text = pytesseract.image_to_string(Image.open('card.jpg'))
+parser = argparse.ArgumentParser(description="Process an image file.")
+parser.add_argument("filename", nargs = "?", type=Path, default = "sample_text_1.jpg", help="Path to the image file")
+args = parser.parse_args()
+
+text = pytesseract.image_to_string(Image.open(args.filename))
 name = re.search(r'^[A-Z][a-z]+\s[A-Z][a-z]+', text, re.M)
 email = re.search(r'[\w\.-]+@[\w\.-]+', text)
+print("text:", text)
 print("Name:", name.group(0) if name else "-")
 print("Email:", email.group(0) if email else "-")
